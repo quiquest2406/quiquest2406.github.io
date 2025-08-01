fc5fc3995b2e1eefa5109e264da6eaef72404032
quiquest2406 <earg34@gmail.com>
2025-08-01 17:44:38 -0400

    2do commit

---
+++ src/Component.jsx
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+
+function Component() {
+
+    const [text,setText]= useState()
+    const [updated,setUpdated]= useState()
+
+    const textOnChange =(event) => {
+        setText(event.target.value)
+    }
+
+     const buttonOnClick =() => {
+        setUpdated(text)
+    }
+
+
+    return (
+        <div>
+            <input type="text" value={text} onChange={textOnChange}/>
+            <button onClick={buttonOnClick}>Actualizar</button> 
+            <p>Texto input {text}</p>
+            <p>Texto Actualizar: {updated}</p>
+        </div>
+    )
+}
+
+export default Component
